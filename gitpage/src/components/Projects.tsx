56b70bbe9483161d8f308b3715ac1b4a0eb2822b
noiryouxi <noiryouxi@gmail.com>
2025-08-05 21:01:53 +0900

    3
    내용 수정

--- gitpage/src/components/Projects.tsx
+++ gitpage/src/components/Projects.tsx
@@ -5,34 +5,10 @@ const Projects: React.FC = () => {
     <section>
       <h2>My Projects</h2>
       <div>
-        <h3>Project 1: Portfolio Website</h3>
+        <h3>서비스 관제 프로그램 개발 (C# + WPF)</h3>
         <p>
-          A personal portfolio website built with React, Next.js, and TypeScript to showcase my skills 
-          and projects.
-        </p>
-        <a href="https://yourportfolio.com" target="_blank" rel="noopener noreferrer">
-          View Project
-        </a>
-      </div>
-      <div>
-        <h3>Project 2: E-commerce App</h3>
-        <p>
-          An e-commerce platform built using React and Node.js. Users can browse products, add them 
-          to the cart, and make payments using Stripe API.
-        </p>
-        <a href="https://ecommerceapp.com" target="_blank" rel="noopener noreferrer">
-          View Project
-        </a>
-      </div>
-      <div>
-        <h3>Project 3: Blogging Platform</h3>
-        <p>
-          A blogging platform built with React and Firebase. Users can create, edit, and delete posts, 
-          and leave comments on each other's blogs.
-        </p>
-        <a href="https://bloggingplatform.com" target="_blank" rel="noopener noreferrer">
-          View Project
-        </a>
+          서비스를 모니터링하면서 문제를 파악하고 조치를 취할수 있는 프로그램을 개발 및 유지보수한 경험이 있습니다.
+        </p>  
       </div>
     </section>
   );
